fa336191859a6e3e42f91e074f1c612f093c4d17
Victor Hasler <victor.hasler@gmail.com>
2025-10-14 09:43:11 +0200

    conf

--- bin/build_cloudregions.sh
+++ bin/build_cloudregions.sh
@@ -16,8 +16,8 @@ LOOKUP_DIR="${APP_BASE}/lookups"
 # -----------------------------------------------------------------------------
 # Pfade
 # -----------------------------------------------------------------------------
-LOCKFILE="${APP_VAR_DIR}/vhc_cloudregions.lock"
-LOGFILE="$SPLUNK_HOME/var/log/splunk/vhc_cloudregions.log"
+LOCKFILE="${APP_VAR_DIR}/vhc_SA_cloudregions.lock"
+LOGFILE="$SPLUNK_HOME/var/log/splunk/vhc_SA_cloudregions.log"
 
 # -----------------------------------------------------------------------------
 # Lockfile: Verhindert parallele Ausführungen
